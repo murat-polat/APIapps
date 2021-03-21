64dbcf7036e264e217dd17976562bd64cb2eac82
Murat Polat <muratsdepo@gmail.com>
2021-03-21 22:38:08 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy 
+app = Flask('__name__')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///depo.db'
+app.config['SQLALCHEMY_TRACK_MOTIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    description = db.Column(db.String(80))
+
+    def __rep__(self):
+        return f"{self.name} - {self.description}"
+
+@app.route('/')
+def index():
+    return ('Hello')
+
+
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data= {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+
+    return {"drinks": output}
+
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+
+    return {'id': drink.id }
+
+
+
+
+if __name__== '__main__':
+    app.run()
